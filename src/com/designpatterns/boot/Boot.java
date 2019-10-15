387ce0c509f0f848adde994696cda5058c5f7b41
Chris Conlon <Chris.Conlon1993@gmail.com>
2019-10-15 19:46:14 -0400

    Hotfix: Removing srvname from Boot.java

--- src/com/designpatterns/boot/Boot.java
+++ src/com/designpatterns/boot/Boot.java
@@ -2,6 +2,6 @@ package com.designpatterns.boot;
 
 public class Boot{
     public static void main(String[] args){
-        System.out.println(System.getProperty("srvname"));
+        System.out.println("Hello World.");
     }
 }
