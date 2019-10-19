e3f910e0c79c7c4c337f0a322373c54885eaf49d
Chris Conlon <Chris.Conlon1993@gmail.com>
2019-10-19 18:31:13 -0400

    Rename boot -> main

--- src/com/designpatterns/main/Main.java
+++ src/com/designpatterns/main/Main.java
@@ -1,4 +1,4 @@
-package com.designpatterns.boot;
+package com.designpatterns.main;
 /**
  * @author Chris Conlon
  * 
@@ -6,7 +6,7 @@ package com.designpatterns.boot;
  * @author John Liu
  * @author Trey DeWald
  */
-public class Boot{
+public class Main{
     public static void main(String[] args){
         System.out.println("Hello World.");
     }
