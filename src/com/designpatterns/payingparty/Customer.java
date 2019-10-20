d23ce77ac2c193c7e37368de191888a06c50f275
Nana Ahiabli <ahiabli.nana@gmail.com>
2019-10-20 14:02:59 -0400

    Vendor class completed

--- src/com/designpatterns/payingparty/Customer.java
+++ src/com/designpatterns/payingparty/Customer.java
@@ -1,20 +1,43 @@
 package com.designpatterns.payingparty;
+import com.designpatterns.sensors.Led;
+import com.designpatterns.accounts.Account;
+import com.designpatterns.accounts.CheckProcessor;
+
 
 public class Customer implements PayingParty{
 
 
-
-    private LED led;
+    /**
+     * 
+     * @param led
+     * @param BankAccount 
+     * @param amount
+     * @param checkProcessor
+     */
+    private Led led;
     private BankAccount checking;
+    private double amount;
     public CheckProcessor checkProcessor;
 
-   
-
+    public Customer(BankAccount checking){
+       this.checking = checking;
+    }
+    /**
+     * 
+     * @return
+     */
     public Account addAccount(){
 
     }
 
-    public void pay(double amount){
-        
+    /**
+     * 
+     * @param amount
+     * @return
+     */
+    public double pay(double amount){
+        this.amount = amount;
+        checkProcessor.processCheck(checking, amount);
+        return amount;
     }
 }
--- src/com/designpatterns/payingparty/Vendor.java
+++ src/com/designpatterns/payingparty/Vendor.java
@@ -1,10 +1,19 @@
 package com.designpatterns.payingparty;
 
+/**
+ * The Vendor class Represents a vendor to whom money is paid when a transaction is completed. 
+ */
 public class Vendor implements PayingParty{ 
 
-    public void pay(double amount){
-        System.out.println("Vendor made payment");
-    }
+    private double amount;
 
+    /**
+     * The pay method alerts the vendor the amount being paid
+     * @param amount The amount of money paid to the vendor
+     */
+    public void pay(double amount){
+        this.amount = amount;
+        System.out.println("Payment made in the amount of $" + amount);
+    }
 
 }
