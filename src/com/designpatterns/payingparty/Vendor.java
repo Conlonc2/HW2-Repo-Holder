dda31e68120325ef6af6fac3dc316c9fdae4618a
Nana Ahiabli <ahiabli.nana@gmail.com>
2019-10-19 23:28:44 -0400

    Added a pay method

--- src/com/designpatterns/payingparty/Vendor.java
+++ src/com/designpatterns/payingparty/Vendor.java
@@ -2,7 +2,7 @@ package com.designpatterns.payingparty;
 
 public class Vendor implements PayingParty{ 
 
-    public void pay(){
+    public void pay(double amount){
         System.out.println("Vendor made payment");
     }
 
