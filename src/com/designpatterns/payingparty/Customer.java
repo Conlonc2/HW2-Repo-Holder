efd4182e21d7e65993541d2ac4dd5c226bf3bcfb
Nana Ahiabli <ahiabli.nana@gmail.com>
2019-10-19 23:14:07 -0400

    Created Customer class and Vendor class

---
+++ src/com/designpatterns/payingparty/Customer.java
@@ -0,0 +1,17 @@
+package com.designpatterns.payingparty;
+
+public class Customer implements PayingParty{
+
+
+
+    private LED led;
+    private BankAccount checking;
+    public CheckProcessor checkProcessor;
+
+   
+
+    public Account addAccount(){
+
+    }
+
+}
---
+++ src/com/designpatterns/payingparty/Vendor.java
@@ -0,0 +1,10 @@
+package com.designpatterns.payingparty;
+
+public class Vendor implements PayingParty{ 
+
+    public void pay(){
+        System.out.println("Vendor made payment");
+    }
+
+
+}
