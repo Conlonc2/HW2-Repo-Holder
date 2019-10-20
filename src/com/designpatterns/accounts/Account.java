b72aa7d2bdb4dcc3638e32a8fde7611394cac9d8
Chris Conlon <Chris.Conlon1993@gmail.com>
2019-10-20 07:30:09 -0400

    Added some documentaion to accounts package/class

--- src/com/designpatterns/accounts/Account.java
+++ src/com/designpatterns/accounts/Account.java
@@ -3,19 +3,39 @@ package com.designpatterns.accounts;
 import com.designpatterns.exceptions.InsufficentFundsException;
 import com.designpatterns.sensors.Led;
 
+/**
+ * The abstract Account class is used as a helper handler. Anything that extends this class will implemnt the deduct method.
+ */
 public abstract class Account {
     public Led identity;
     protected double total;
     protected double toDeduct;
     protected Account next;
 
+    /**
+     * Constructor for Account:
+     * @param identifier String: The name of the account being created (i.e. checkings/savings)
+     * @param balance double: the initial balance to set the account up with.
+     */
     public Account(String identifier, double balance) throws com.designpatterns.exceptions.InsufficentFundsException {
         identity = new Led(identifier);
         this.total = balance;
     }
-
+    /**
+     * 
+     * @param amount double: How much to deduct from account. To be implemnted by children
+     */
     public abstract void deduct(double amount);
 
+    /**
+     * This method will deduct the amount it can help with from the customer request.
+     * If account has enough to fund the customer request deduct from thsi account.
+     * If the balance is to low and there is a next account to check set a flag as to how to remove. This will
+     * remove the amount as long as the customer transaction is satisfied.
+     * Else we will throw an InsufficentFundsException.
+     * @param remainder double: The amount a customer requires from the account.
+     * @throws InsufficentFundsException
+     */
     public void validateFunds(double remainder) throws InsufficentFundsException {
         double test =  this.total - remainder;
         System.out.println(test);
@@ -29,6 +49,7 @@ public abstract class Account {
             this.deduct(toDeduct);
         }
         else{
+            this.identity.toggle();
             throw new InsufficentFundsException("Not enough funds!");
         }
     }
